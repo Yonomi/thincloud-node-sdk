7e935010af60e4d00254238d5ae87e2698b77f45
Erica Selis <erica.selis@gmail.com>
2017-06-26 14:14:00 -0300

    fix MessageProcessor

--- src/utils/messageProcessor.js
+++ src/utils/messageProcessor.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const Utils = require('./index');
+const Topic = require('./topic');
+const Constants = require('./constants');
+const Message = require('./message');
+const Command = require('./command');
 const uuid = require('uuid');
 
 class MessageProcessor {
@@ -15,27 +18,27 @@ class MessageProcessor {
   }
 
   process(topic, payload) {
-    const _topic = new Utils.Topic(topic);
+    const _topic = new Topic(topic);
     let json;
 
     try {
       json = JSON.parse(payload.toString());
     } catch (e) {
-      return this._client.eventSource.emit('error', new Utils.Message(uuid.v4(), {message: 'invalid json'}, 'error', 400).toJSON());
+      return this._client.eventSource.emit('error', new Message(uuid.v4(), {message: 'invalid json'}, 'error', 400).toJSON());
     }
 
-    if (_topic.type === Utils.Constants.Topic.REGISTRATION) {
+    if (_topic.type === Constants.Topic.REGISTRATION) {
       this._client.deviceId = json.result.deviceId || null;
-      this.eventSource.emit(Utils.Constants.DeviceEvents.DEVICE_SUBSCRIPTION, json);
+      this.eventSource.emit(Constants.DeviceEvents.DEVICE_SUBSCRIPTION, json);
     }
 
-    if (_topic.type === Utils.Constants.Topic.DEVICE && _topic.action === Utils.Constants.Topic.DEVICE_REQUEST) {
-      this.eventSource.emit(Utils.Constants.DeviceEvents.DEVICE_REQUEST, json);
-      this.eventSource.emit(`${Utils.Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, json);
+    if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_REQUEST) {
+      this.eventSource.emit(Constants.DeviceEvents.DEVICE_REQUEST, json);
+      this.eventSource.emit(`${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, json);
     }
 
-    if (_topic.type === Utils.Constants.Topic.DEVICE && _topic.action === Utils.Constants.Topic.DEVICE_COMMAND) {
-      this.eventSource.emit(Utils.Constants.DeviceEvents.DEVICE_COMMAND, new Utils.Command(json, this._client));
+    if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_COMMAND) {
+      this.eventSource.emit(Constants.DeviceEvents.DEVICE_COMMAND, new Command(json, this._client));
     }
   }
 }
