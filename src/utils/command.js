42f83f92d7a5729dcad04a6654723b6438fed962
Hassan Khokhar <hassan@yonomi.co>
2017-06-29 14:40:54 -0600

    minor fix for commands and restructured logs a bit

--- src/utils/command.js
+++ src/utils/command.js
@@ -6,8 +6,8 @@ const log = require('./logger');
 
 class Command {
   constructor(command, client){
-    this.validate(command, client);
     Object.assign(this, command);
+    this.validate();
     this._self = command;
     this._client = client;
     this._topic = new CommandTopic(this._client.deviceId, this.id);
@@ -20,19 +20,20 @@ class Command {
 
   success(payload) {
     return new Promise((resolve, reject)=> {
-      this._client.eventSource.publish(this._topic.response, new Message(this.id, payload, 'success', 200).toString(), (err)=> {
+      let message = new Message(this.id, payload, 'success', 200);
+      this._client.eventSource.publish(this._topic.response, message.toString(), (err)=> {
         if(err) reject(err);
-        else resolve()
+        else resolve(message.toJSON())
       });
     });
   }
 
   error(payload){
     return new Promise((resolve, reject)=> {
-      log.info('publish a msg error');
-      this._client.eventSource.publish(this._topic.response, new Message(this.id, payload, 'error', 400).toString(), (err)=> {
+      let message = new Message(this.id, payload, 'error', 400);
+      this._client.eventSource.publish(this._topic.response, message.toString(), (err)=> {
         if(err) reject(err);
-        else resolve()
+        else resolve(message.toJSON())
       });
     });
   }
@@ -41,21 +42,15 @@ class Command {
     return this._self;
   }
 
-  validate(request, client) {
-    if(!request) {
-      log.info(`receive a command with an error: json can not be empty`);
-      client.eventSource.emit('error', new Message(this.id, {message: 'json can not be empty'}, 'error', 400).toJSON());
-      return this.error(JSON.stringify(`{message: 'json can not be empty'}`));
-    }
-    if(request && !request.id) {
-      log.info(`receive a command with an error: id can not be empty`);
-      client.eventSource.emit('error', new Message(this.id, {message: 'id can not be empty'}, 'error', 400).toJSON());
-      return this.error(JSON.stringify(`{message: 'id can not be empty'}`));
-    }
-    if(request && !request.method) {
-      log.info(`receive a command with an error: method can not be empty`);
-      client.eventSource.emit('error', new Message(this.id, {message: 'method can not be empty'}, 'error', 400).toJSON());
-      return this.error(JSON.stringify(`{message: 'method can not be empty'}`));
+  get _isValid(){
+    return this.id && this.method
+  }
+
+  validate() {
+    if(!this._isValid){
+      throw Error('commandValidationException', 'missing parameters, id, method');
+    } else {
+      return this._isValid;
     }
   }
 
--- src/utils/messageProcessor.js
+++ src/utils/messageProcessor.js
@@ -21,45 +21,38 @@ class MessageProcessor {
   process(topic, payload) {
     const _topic = new Topic(topic);
     let json;
-    log.info(`get a json from ${topic} and is ${payload.toString()}`);
+    log.info({eventType : 'message', topic : _topic, data : payload});
 
     try {
       json = JSON.parse(payload.toString());
     } catch (e) {
-      log.info(`invalid json from ${topic}, json data: ${payload.toString()}`);
-      return this._client.eventSource.emit('error', new Message(uuid.v4(), {message: 'invalid json'}, 'error', 400).toJSON());
+      let message = new Message(uuid.v4(), {message: 'invalid json'}, 'error', 400);
+      log.error({eventType : 'emit', topic : _topic, data: message.toString()});
+      return this._client.eventSource.emit('error', message.toJSON());
     }
 
     if (_topic.type === Constants.Topic.REGISTRATION) {
       this._client.deviceId = json.result.deviceId || null;
-      log.info(`will emit a subscription event`);
+      log.info({eventType : 'emit', topic : _topic, data : json, listener : Constants.DeviceEvents.DEVICE_SUBSCRIPTION});
       this.eventSource.emit(Constants.DeviceEvents.DEVICE_SUBSCRIPTION, json);
     }
 
     if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_REQUEST) {
-      log.info(`will emit a device event`);
+      log.info({eventType : 'emit', topic : _topic, data : json, listener : Constants.DeviceEvents.DEVICE_REQUEST});
+      log.info({eventType : 'emit', topic : _topic, data : json, listener : `${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`);
       this.eventSource.emit(Constants.DeviceEvents.DEVICE_REQUEST, json);
       this.eventSource.emit(`${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, json);
     }
 
     if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_COMMAND) {
-      log.info(`will emit a device command event`);
-      /*
-       TODO: the command object passed into the event on next line needs to be wrapped in try catch before we pass it
-       TODO: to the event emitter. also, need to change the command, so that rather then sending a message there, we are
-       TODO: going to throw the error from the command.js if the validation failed and catch here then send an error instead
-       */
-      // // something like this
-      // try {
-      //// the real deal is that the command show throw an error when its initialized as an object that it failed validation
-      //   let command = new Command(json, this._client);
-      //   this.eventSource.emit(Constants.DeviceEvents.DEVICE_COMMAND, new Command(json, this._client));
-      // } catch (ex => {
-      //   this.eventSource.emit('error', {}.toString());
-      // });
+      try {
+        let command = new Command(json, this._client);
+        log.info({eventType : 'emit', topic : _topic, data : json, listener : Constants.DeviceEvents.DEVICE_COMMAND});
+        this.eventSource.emit(Constants.DeviceEvents.DEVICE_COMMAND, command);
+      } catch(err){
+        log.error({eventType: 'command', topic : _topic, data: err})
+      };
 
-
-      this.eventSource.emit(Constants.DeviceEvents.DEVICE_COMMAND, new Command(json, this._client));
     }
   }
 }
