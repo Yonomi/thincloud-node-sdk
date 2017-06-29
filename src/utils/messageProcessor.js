4df27075615516670e993614596a9d461bea633a
Hassan Khokhar <hassan@yonomi.co>
2017-06-29 14:49:15 -0600

    logs fix

--- src/utils/messageProcessor.js
+++ src/utils/messageProcessor.js
@@ -21,25 +21,25 @@ class MessageProcessor {
   process(topic, payload) {
     const _topic = new Topic(topic);
     let json;
-    log.info({eventType : 'message', topic : _topic, data : payload});
+    log.info({eventType : 'message', topic : _topic.toString(), data : payload.toString()});
 
     try {
       json = JSON.parse(payload.toString());
     } catch (e) {
       let message = new Message(uuid.v4(), {message: 'invalid json'}, 'error', 400);
-      log.error({eventType : 'emit', topic : _topic, data: message.toString()});
+      log.error({eventType : 'emit', topic : _topic.toString(), data: message.toString()});
       return this._client.eventSource.emit('error', message.toJSON());
     }
 
     if (_topic.type === Constants.Topic.REGISTRATION) {
       this._client.deviceId = json.result.deviceId || null;
-      log.info({eventType : 'emit', topic : _topic, data : json, listener : Constants.DeviceEvents.DEVICE_SUBSCRIPTION});
+      log.info({eventType : 'emit', topic : _topic.toString(), listener : Constants.DeviceEvents.DEVICE_SUBSCRIPTION, data : json});
       this.eventSource.emit(Constants.DeviceEvents.DEVICE_SUBSCRIPTION, json);
     }
 
     if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_REQUEST) {
-      log.info({eventType : 'emit', topic : _topic, data : json, listener : Constants.DeviceEvents.DEVICE_REQUEST});
-      log.info({eventType : 'emit', topic : _topic, data : json, listener : `${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`});
+      log.info({eventType : 'emit', topic : _topic.toString(), data : json, listener : Constants.DeviceEvents.DEVICE_REQUEST});
+      log.info({eventType : 'emit', topic : _topic.toString(), listener : `${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, data : json});
       this.eventSource.emit(Constants.DeviceEvents.DEVICE_REQUEST, json);
       this.eventSource.emit(`${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, json);
     }
@@ -47,10 +47,10 @@ class MessageProcessor {
     if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_COMMAND) {
       try {
         let command = new Command(json, this._client);
-        log.info({eventType : 'emit', topic : _topic, data : json, listener : Constants.DeviceEvents.DEVICE_COMMAND});
+        log.info({eventType : 'emit', topic : _topic.toString(), listener : Constants.DeviceEvents.DEVICE_COMMAND, data : json});
         this.eventSource.emit(Constants.DeviceEvents.DEVICE_COMMAND, command);
       } catch(err){
-        log.error({eventType: 'command', topic : _topic, data: err})
+        log.error({eventType: 'command', topic : _topic.toString(), data: err})
       }
     }
   }
--- src/utils/topic.js
+++ src/utils/topic.js
@@ -3,11 +3,16 @@
 class Topic {
 
   constructor(topic) {
+    this._self = topic;
     this.type = topic.split('/')[1];
     this.deviceId = topic.split('/')[2];
     this.action = topic.split('/')[3];
     //actionId is the request or the commandId
     this.actionId = topic.split('/')[4];
+  }
+
+  toString(){
+    return this._self;
   }
   
 }
