02259278c0990e442ccf42ea7b0df2017ecb3a71
Hassan Khokhar <hassan@yonomi.co>
2017-06-29 14:58:12 -0600

    minor cleanup on logs

--- src/utils/messageProcessor.js
+++ src/utils/messageProcessor.js
@@ -33,21 +33,21 @@ class MessageProcessor {
 
     if (_topic.type === Constants.Topic.REGISTRATION) {
       this._client.deviceId = json.result.deviceId || null;
-      log.info({eventType : 'emit', topic : _topic.toString(), listener : Constants.DeviceEvents.DEVICE_SUBSCRIPTION, data : json});
+      log.info({eventType : 'emit', listener : Constants.DeviceEvents.DEVICE_SUBSCRIPTION, data : json});
       this.eventSource.emit(Constants.DeviceEvents.DEVICE_SUBSCRIPTION, json);
     }
 
     if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_REQUEST) {
-      log.info({eventType : 'emit', topic : _topic.toString(), data : json, listener : Constants.DeviceEvents.DEVICE_REQUEST});
-      log.info({eventType : 'emit', topic : _topic.toString(), listener : `${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, data : json});
+      log.info({eventType : 'emit', listener : Constants.DeviceEvents.DEVICE_REQUEST, data : json});
       this.eventSource.emit(Constants.DeviceEvents.DEVICE_REQUEST, json);
+      log.info({eventType : 'emit', listener : `${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, data : json});
       this.eventSource.emit(`${Constants.DeviceEvents.DEVICE_REQUEST}/${json.id}`, json);
     }
 
     if (_topic.type === Constants.Topic.DEVICE && _topic.action === Constants.Topic.DEVICE_COMMAND) {
       try {
         let command = new Command(json, this._client);
-        log.info({eventType : 'emit', topic : _topic.toString(), listener : Constants.DeviceEvents.DEVICE_COMMAND, data : json});
+        log.info({eventType : 'emit', listener : Constants.DeviceEvents.DEVICE_COMMAND, data : json});
         this.eventSource.emit(Constants.DeviceEvents.DEVICE_COMMAND, command);
       } catch(err){
         log.error({eventType: 'command', topic : _topic.toString(), data: err})
